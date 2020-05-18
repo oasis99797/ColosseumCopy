3428de744fb5113fc59ac4964485fa0c9383d72e
oasis99797 <oasis99797@gmail.com>
2020-05-18 11:08:08 +0900

    프사 선택 => 가져온 프사 뿌려주기

--- app/src/main/java/com/bklee/colosseumcopy/MainActivity.kt
+++ app/src/main/java/com/bklee/colosseumcopy/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.bklee.colosseumcopy
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    val REQ_FOR_PICK_IMAGE_FROM_GALLERY = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +21,26 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-     }
+
+        changeProfileBtn.setOnClickListener {
+            val myIntent = Intent(Intent.ACTION_PICK)
+            myIntent.setType("image/*")
+            myIntent.setType(MediaStore.Images.Media.CONTENT_TYPE)
+            startActivityForResult(myIntent, REQ_FOR_PICK_IMAGE_FROM_GALLERY)
+        }
+    }
 
     override fun setValues() {
-     }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if( requestCode == REQ_FOR_PICK_IMAGE_FROM_GALLERY ) {
+            if( resultCode == Activity.RESULT_OK ) {
+                Glide.with(mContext).load(data?.data).into(myProfileImg)
+            }
+        }
+    }
 
 }
